bd6a0c8570e9e87e8bca15be6bb915d7454b3b46
dadyjoe862 <dadyoo360@gmail.com>
2025-01-11 15:48:43 -0500

    comm

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
+    <div class="bg" >
+        <div class="flex flex-col items-center justify-center px-8 py-8 mx-auto md:h-screen max-md:h-screen">
+            <img src="../assets/food4.jpg" alt="">
+
+            
+
+        </div>
 
     </div>
 </template>
